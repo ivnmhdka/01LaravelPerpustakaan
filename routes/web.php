dc44179700f16a9538bd7c8f3c352e5e62f54130
ivnmhdka <ivanmahadika05@gmail.com>
2022-03-01 10:10:05 +0700

    Modul7

--- routes/web.php
+++ routes/web.php
@@ -32,7 +32,9 @@ Route::get('/gallery', function () {
         "title" => "Gallery"
     ]);
 });
-Route::resource('/contacts', ContactController::class);
+// Route::resource('/contacts', ContactController::class);
+Route::get('/contact/create', [ContactController::class, 'create'])->name('contacts.create');
+Route::post('/contact/store', [ContactController::class, 'store'])->name('contacts.store');
 
 use Illuminate\Support\Facades\Auth;
 
@@ -40,4 +42,8 @@ Auth::routes();
 
 Route::group(['middleware' => ['auth']], function () {
     Route::get('/home', [App\http\Controllers\HomeController::class, 'index'])->name('home');
+    Route::get('/contact/index', [ContactController::class, 'index'])->name('contacts.index');
+    Route::get('/contact/{id}/edit', [ContactController::class, 'edit'])->name('contacts.edit');
+    Route::post('/contact/{id}/update', [ContactController::class, 'update'])->name('contacts.update');
+    Route::get('/contact/{id}/destroy', [ContactController::class, 'destroy'])->name('contacts.destroy');
 });
